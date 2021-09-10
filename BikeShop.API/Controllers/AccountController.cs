3f699df38aec97f31a36fc81a5661f9a946ba569
wimForton <wim.forton@gmail.com>
2021-09-10 16:05:58 +0200

    Api Account Login and Register Working

--- BikeShop.API/Controllers/AccountController.cs
+++ BikeShop.API/Controllers/AccountController.cs
@@ -1,9 +1,15 @@
 ﻿using BikeShop.BLL.Services.Account;
+using BikeShop.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -14,42 +20,53 @@ namespace BikeShop.API.Controllers
     [ApiController]
     public class AccountController : ControllerBase
     {
-        private readonly IAccountService _AccountService;
-        public AccountController(IAccountService AccountService)
+        private readonly IConfiguration _configuration;
+        private readonly UserManager<IdentityUser> _userManager;
+        public AccountController(IConfiguration configuration, UserManager<IdentityUser> userManager)
         {
-            _AccountService = AccountService;
+            _configuration = configuration;
+            _userManager = userManager;
         }
-        //GET: api/<AccountController>
-        [HttpGet]
-        public IEnumerable<IdentityUser> Get()
+        [HttpPost, Route("login")]
+        public async Task<IActionResult> Login([FromBody] LoginViewModel login)
         {
-            return _AccountService.GetAccounts();
+            if (login == null)
+            {
+                return BadRequest("Invalid client request");
+            }
+            var user = await _userManager.FindByNameAsync(login.UserName);
+            if (user != null && await _userManager.CheckPasswordAsync(user, login.Password))
+            {
+                var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Secret"]));
+                var signinCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+                var tokenOptions = new JwtSecurityToken(
+                    issuer: "http://localhost:44387",
+                    audience: "http://localhost:44387",
+                    claims: new List<Claim>(),
+                    expires: DateTime.Now.AddMinutes(5),
+                    signingCredentials: signinCredentials
+                );
+                var tokenString = new JwtSecurityTokenHandler().WriteToken(tokenOptions);
+                return Ok(new { token = tokenString, user = user });
+            }
+            else
+            {
+                return Unauthorized();
+            }
         }
 
-        // GET api/<AccountController>/5
-        [HttpGet("{id}")]
-        public async Task<IdentityUser> Get(string id)
-        {
-            return await _AccountService.GetAccountByIdAsync(id);
-        }
-
-        // POST api/<AccountController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        [Route("register")]
+        public IActionResult Register([FromBody] LoginViewModel model)
         {
+            var user = new IdentityUser { UserName = model.UserName, PasswordHash = model.Password };
+            var result = _userManager.CreateAsync(user, model.Password);
+            if (!result.Result.Succeeded)
+            {
+                return BadRequest(result.Result.Errors);
+            }
 
-        }
-
-        // PUT api/<AccountController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/<AccountController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            return Ok();
         }
     }
 }
